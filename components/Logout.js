64474018dbcaa443a3d9dbd312f12396c81a4a80
puloxtor <kotzegien@gmail.com>
2024-09-22 17:19:23 +0300

    first commit im learning react and git

--- components/Logout.js
+++ components/Logout.js
@@ -16,7 +16,7 @@ export default function Logout() {
     if (pathname === '/') {
         return (
             <Link href={'/dashboard'}>
-                <Button text="Go to dashboard" />
+                <Button text="Logout" />
             </Link>
         )
     }
